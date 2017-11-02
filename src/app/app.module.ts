5ec4660096f34b06994a4efb16728f41f8314900
orlovskiy_k <orlovskiy_k@lwo.by>
2017-11-02 17:03:13 +0300

    storeStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import {StaticDataSource} from "./model/static.datasource";
+import {ProductRepository} from "./model/product.repository";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [StaticDataSource, ProductRepository],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
